334d371b48aa793173ddc29a61402b5716789847
Jeffrey Shen <jeffdshen@gmail.com>
2021-03-10 08:20:06 -0800

    Fix test script

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ import util
 def add_subparser(name, data_sub_dir, subparsers, parent_parser, module):
     subparser = subparsers.add_parser(name, parents=[parent_parser])
     module.add_test_args(subparser)
-    subparser.set_defaults(train=module.test)
+    subparser.set_defaults(test=module.test)
     subparser.set_defaults(data_sub_dir=data_sub_dir)
 
 
